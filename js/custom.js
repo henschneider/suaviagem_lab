6806a54241921f77c7c0a0a98fb75c04fb13ed9a
Henrique Schneider <stankievicz@gmail.com>
2017-06-18 19:54:15 -0300

    Alterado nome do arquivo JS
    Arquivo com as customizações.

---
+++ js/custom.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+
+$( document ).ready(function(){
+    $(".button-collapse").sideNav();
+});
+
+
+$('input.autocomplete').autocomplete({
+    data: {
+      "Apple": null,
+      "Microsoft": null,
+      "Google": 'http://placehold.it/250x250'
+    },
+    limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
+    onAutocomplete: function(val) {
+      // Callback function when value is autcompleted.
+    },
+    minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
+  });
+        
